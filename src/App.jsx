1c90a43093d88f1455d7cb356efee090ef4bd530
Shivam Singh Rawat <shivamrawat2000@gmail.com>
2021-05-08 21:29:23 +0530

    fixed useEffect has missing dependencies which caused issues in deployment

--- src/App.jsx
+++ src/App.jsx
@@ -14,29 +14,29 @@ function App() {
   }, []);
 
   useEffect(() => {
+    const filterHandler = () => {
+      switch (status) {
+        case "completed":
+          setFilteredTodos(todos.filter((todo) => todo.completed === true));
+          break;
+
+        case "incomplete":
+          setFilteredTodos(todos.filter((todo) => todo.completed === false));
+          break;
+
+        default:
+          setFilteredTodos(todos);
+          break;
+      }
+    };
+
+    const saveLocalTodos = () => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    };
+
     filterHandler();
     saveLocalTodos();
   }, [todos, status]);
-
-  const filterHandler = () => {
-    switch (status) {
-      case "completed":
-        setFilteredTodos(todos.filter((todo) => todo.completed === true));
-        break;
-
-      case "incomplete":
-        setFilteredTodos(todos.filter((todo) => todo.completed === false));
-        break;
-
-      default:
-        setFilteredTodos(todos);
-        break;
-    }
-  };
-
-  const saveLocalTodos = () => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  };
 
   const getLocalTodos = () => {
     if (localStorage.getItem("todos") === null) {
